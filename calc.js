c544ae4626292daf21efe0f4edff242f4c87b516
Morgen Stary <morgenstary@North-Pole.local>
2018-09-26 14:20:32 -0700

    .js created

---
+++ calc.js
@@ -0,0 +1,40 @@
+$(".form-button").on("click", function (event) {
+    var hashrate = $('#hashrate').val();
+    event.preventDefault();
+
+    $("#hashrate").val();
+
+    // Not sure how to implement this method within my code???
+    // var num = 5.56789;
+    // var n = num.toFixed(2);
+
+    var queryURL = "https://alloscomp.com/bitcoin/calculator/json?hashrate=" + hashrate;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        $("#difficulty").html(response.difficulty);
+
+        $("#exchange_rate").html('$ ' + response.exchange_rate);
+
+        $("#bc_per_block").html(response.bc_per_block);
+
+        $("#btc_day").html('₿ ' + response.coins_per_hour * 24 * 1000000000);
+
+        $("#btc_week").html('₿ ' + response.coins_per_hour * 24 * 7 * 1000000000);
+
+        $("#btc_month").html('₿ ' + response.coins_per_hour * 24 * 7 * 4 * 1000000000);
+
+        $("#dol_day").html('$ ' + response.dollars_per_hour * 24 * 1000000000);
+
+        $("#dol_week").html('$ ' + response.dollars_per_hour * 24 * 7 * 1000000000);
+
+        $("#dol_month").html('$ ' + response.dollars_per_hour * 24 * 7 * 4 * 1000000000);
+
+        console.log(response);
+
+    });
+
+});
