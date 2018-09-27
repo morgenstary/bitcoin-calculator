49f0044e0c309972950ea8b8005289237d4324a6
Morgen Stary <morgenstary@North-Pole.local>
2018-09-26 21:52:19 -0700

    yes

--- calc.js
+++ calc.js
@@ -7,10 +7,6 @@ $("#submitHashrate").on("click", function (event) {
     event.preventDefault();
     var hashrate = $('#hashrate').val();
 
-    // Not sure how to implement this method within my code???
-    // var num = 5.56789;
-    // var n = num.toFixed(2);
-
     var queryURL = "https://alloscomp.com/bitcoin/calculator/json?hashrate=" + hashrate;
 
     $.ajax({
@@ -24,11 +20,11 @@ $("#submitHashrate").on("click", function (event) {
 
         $("#bc_per_block").html(response.bc_per_block);
 
-        $("#btc_day").html('₿ ' + response.coins_per_hour * 24 * 1000000000);
+        $("#btc_day").html('₿ ' + (response.coins_per_hour * 24 * 1000000000).toFixed(6));
 
-        $("#btc_week").html('₿ ' + response.coins_per_hour * 24 * 7 * 1000000000);
+        $("#btc_week").html('₿ ' + (response.coins_per_hour * 24 * 7 * 1000000000).toFixed(6));
 
-        $("#btc_month").html('₿ ' + response.coins_per_hour * 24 * 7 * 4 * 1000000000);
+        $("#btc_month").html('₿ ' + (response.coins_per_hour * 24 * 7 * 4 * 1000000000).toFixed(6));
 
         $("#dol_day").html('$ ' + (response.dollars_per_hour * 24 * 1000000000).toFixed(2));
 
